cc9d7aa00bb1331adebd6b9a4d9e4f651dd66fec
Arturich <artursavchuk7@gmail.com>
2020-11-25 22:15:33 +0200

    Server was added

---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+from _thread import *
+import _pickle as pickle
+import time
+import random
+import math
+
+# setup sockets
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+S.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Set constants
+PORT = 5555
+
+BALL_RADIUS = 5
+START_RADIUS = 7
+
+ROUND_TIME = 60 * 5
+
+MASS_LOSS_TIME = 1
+
+W, H = 1600, 830
+
+HOST_NAME = socket.gethostname()
+SERVER_IP = socket.gethostbyname(HOST_NAME)
+
+try:
+    S.bind((SERVER_IP, PORT))
+except socket.error as e:
+    print(str(e))
+    print("[SERVER] Server could not start")
+    quit()
+
+S.listen()  # listen for connections
+
+print(f"[SERVER] Server Started with local ip {SERVER_IP}")
+
+# dynamic variables
+players = {}
+balls = []
+connections = 0
+_id = 0
+colors = [(255,0,0), (255, 128, 0), (255,255,0), (128,255,0),(0,255,0),(0,255,128),(0,255,255),(0, 128, 255), (0,0,255), (0,0,255), (128,0,255),(255,0,255), (255,0,128),(128,128,128), (0,0,0)]
+start = False
+stat_time = 0
+game_time = "Starting Soon"
+nxt = 1
