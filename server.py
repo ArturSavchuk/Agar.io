9e3a649774f451403245c3e0330473f2622b599b
Arturich <artursavchuk7@gmail.com>
2020-11-26 00:17:52 +0200

    Added thread/player logic

--- server.py
+++ server.py
@@ -60,6 +60,55 @@ def release_mass(players):
 			p["score"] = math.floor(p["score"]*0.95)
 
 
+
+def create_balls(balls, n):
+	"""
+	creates orbs/balls on the screen
+
+	:param balls: a list to add balls/orbs to
+	:param n: the amount of balls to make
+	:return: None
+	"""
+	for i in range(n):
+		while True:
+			stop = True
+			x = random.randrange(0,W)
+			y = random.randrange(0,H)
+			for player in players:
+				p = players[player]
+				dis = math.sqrt((x - p["x"])**2 + (y-p["y"])**2)
+				if dis <= START_RADIUS + p["score"]:
+					stop = False
+			if stop:
+				break
+
+		balls.append((x,y, random.choice(colors)))
+		
+
+def player_collision(players):
+	"""
+	checks for player collision and handles that collision
+
+	:param players: dict
+	:return: None
+	"""
+	sort_players = sorted(players, key=lambda x: players[x]["score"])
+	for x, player1 in enumerate(sort_players):
+		for player2 in sort_players[x+1:]:
+			p1x = players[player1]["x"]
+			p1y = players[player1]["y"]
+
+			p2x = players[player2]["x"]
+			p2y = players[player2]["y"]
+
+			dis = math.sqrt((p1x - p2x)**2 + (p1y-p2y)**2)
+			if dis < players[player2]["score"] - players[player1]["score"]*0.85:
+				players[player2]["score"] = math.sqrt(players[player2]["score"]**2 + players[player1]["score"]**2) # adding areas instead of radii
+				players[player1]["score"] = 0
+				players[player1]["x"], players[player1]["y"] = get_start_location(players)
+				print(f"[GAME] " + players[player2]["name"] + " ATE " + players[player1]["name"])
+
+
 def check_collision(players, balls):
 	"""
 	checks if any of the player have collided with any of the balls
